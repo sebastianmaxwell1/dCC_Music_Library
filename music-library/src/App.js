9b9bb9f70f7976747ba5547cf14ed6b61d6e1794
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-11-03 22:50:04 -0400

    Started NavigationBar

--- music-library/src/App.js
+++ music-library/src/App.js
@@ -1,12 +1,20 @@
+import React, { Component } from 'react';
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
 
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+        <div>
+          <NavigationBar />
+        </div>
+     );
+  }
 }
-
+ 
 export default App;
---
+++ music-library/src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavigationBar(props) {
+    return (
+        <div className="navbar">
+                    <h1>Music Library</h1>
+        </div>
+    );
+}
+
+export default NavigationBar;
